d9c0c734cb20a7c27380504f5942f09a5e03f9f3
Payel-012 <129990158+Payel-012@users.noreply.github.com>
2023-04-06 13:55:04 +0600

    Add files via upload

---
+++ 1002.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main ()
+{
+
+    double A,R;
+
+    scanf("%1f",&R);
+
+    A=3.14159*R*R;
+
+
+    printf("A+%.41f\n",A);
+
+
+    return 0 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
